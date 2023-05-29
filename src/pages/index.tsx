9c9a808782720e844a1f83ebc13ce89f8da8f9d0
Bits-Logic <paradiseofgraphics@gmail.com>
2023-05-30 00:01:35 +0500

    some improvement

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -13,7 +13,7 @@ export default function index() {
     <Head>
     <title>EDDY: Shopping Store</title>
     <meta name="description" content="A shopping Store with nextjs" />
-    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1.0, user-scalable=no" />
     <link rel="icon" type="image/png"  href="/icon.png" /> 
 </Head>
 <main className={styles.main} >
